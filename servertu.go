9241f1c78ecdcb3aa4a4fc175d964df2bd66b51d
fops9311 <fops9311@yandex.ru>
2023-09-19 14:19:12 +0300

    Read port fix

--- servertu.go
+++ servertu.go
@@ -1,6 +1,7 @@
 package mbserver
 
 import (
+	"bufio"
 	"io"
 	"log"
 
@@ -26,7 +27,7 @@ func (s *Server) ListenRTU(serialConfig *serial.Config) (err error) {
 }
 
 func (s *Server) acceptSerialRequests(port serial.Port) {
-	SkipFrameError:
+SkipFrameError:
 	for {
 		select {
 		case <-s.portsCloseChan:
@@ -34,9 +35,8 @@ func (s *Server) acceptSerialRequests(port serial.Port) {
 		default:
 		}
 
-		buffer := make([]byte, 512)
-
-		bytesRead, err := port.Read(buffer)
+		buffer, _, err := bufio.NewReader(port).ReadLine()
+		bytesRead := len(buffer)
 		if err != nil {
 			if err != io.EOF {
 				log.Printf("serial read error %v\n", err)
@@ -52,7 +52,7 @@ func (s *Server) acceptSerialRequests(port serial.Port) {
 			frame, err := NewRTUFrame(packet)
 			if err != nil {
 				log.Printf("bad serial frame error %v\n", err)
-				//The next line prevents RTU server from exiting when it receives a bad frame. Simply discard the erroneous 
+				//The next line prevents RTU server from exiting when it receives a bad frame. Simply discard the erroneous
 				//frame and wait for next frame by jumping back to the beginning of the 'for' loop.
 				log.Printf("Keep the RTU server running!!\n")
 				continue SkipFrameError
